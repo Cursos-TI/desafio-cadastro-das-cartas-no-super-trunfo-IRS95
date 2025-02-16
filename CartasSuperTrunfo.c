ba9f6bd65275ebd3b99fc70a54ffa8b27b05274e
IRS95 <igorrs777@gmail.com>
2025-02-16 14:46:05 +0000

    Nivel Básico

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,21 +2,41 @@
 
 int main() {
     char estado;
-    char codigodacarta[50];
-    char nomedacidade[50];
+    char codigo[50];
+    char cidade[50];
     int populacao;
-    float areaemkm²;
+    float area;
     float pib;
-    int numerodepontosturisticos;
+    int pontosturisticos;
     
+    printf("Digite seu estado: \n");
+    scanf("%s", &estado);
+    
+    printf("Digite o código da carta: \n");
+    scanf("%d", &codigo);
+    
+    printf(" Digite o nome da cidade: \n");
+    scanf("%s", &cidade);
 
+    printf("Digite a população: \n");
+    scanf("%d", &populacao);
 
+    printf("Digite a área em km²: \n");
+    scanf("%f", &area);
 
+    printf("Digite o PIB: \n");
+    scanf("%f", &pib);
 
+    printf("Número de pontos turisticos: \n");
+    scanf("&d", &pontosturisticos);
 
+    printf("Nome do estado: %s", estado);
+    printf("Número do código: %d", codigo);
+    printf("Nome da cidade: %s", cidade);
+    printf("Número de habitantes: %d", populacao);
+    printf("Área em km²: %f", area);
+    printf("Produto interno bruto: %f", pib);
+    printf("Número de pontos turisticos: %d", pontosturisticos);
 
-
-
-
-
-    }
+    return 0;
+}
